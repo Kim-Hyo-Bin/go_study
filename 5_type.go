5a63178217d6f63cd7002801ec575d11574704fd
HyoBin Kim <hb.kim@okestro.com>
2024-01-11 16:49:47 +0900

    go type
    type and example

---
+++ 5_type.go
@@ -0,0 +1,13 @@
+package main
+
+import ("fmt"
+ "reflect")
+func main() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(j), reflect.TypeOf(k), reflect.TypeOf(c) ,reflect.TypeOf(python) ,reflect.TypeOf(java))
+}
+
