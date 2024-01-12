f3aa277c9447eb2f6f43f68367584b653a1926f8
okestro-hbkim <51896679+Kim-Hyo-Bin@users.noreply.github.com>
2024-01-12 14:13:11 +0000

    rename 006
    6_routine.go -> 006_routine.go
    
    rename for file sorting

---
+++ 006_routine.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "runtime"
+    "sync"
+)
+
+type counter struct {
+    i int64
+    mu sync.Mutex
+}
+
+func (c *counter) increment() {
+    c.mu.Lock()
+    c.i += 1
+    c.mu.Unlock()
+}
+
+func main() {
+    runtime.GOMAXPROCS(runtime.NumCPU())
+
+    c := counter{i: 0}
+    wg := sync.WaitGroup{}
+
+    for i := 0; i < 1000; i++ {
+        wg.Add(1)
+	if i%100==0 {
+	    fmt.Println(i)
+	}
+        go func() {
+            defer wg.Done()
+            c.increment()
+        }()
+    }
+
+    wg.Wait()
+    fmt.Println(c.i)
+}
