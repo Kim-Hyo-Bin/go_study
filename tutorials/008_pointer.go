4b455786cf566e62675bb35bfd996164626b2066
HyoBin Kim <hb.kim@okestro.com>
2024-01-16 10:56:29 +0900

    go pointer
    pointer tutorial & example

---
+++ tutorials/008_pointer.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type MyType struct {
+    Value int
+}
+
+func (v MyType) SetValue(newValue int) {
+    v.Value = newValue
+}
+
+func (p *MyType) SetValueWithPointer(newValue int) {
+    p.Value = newValue
+}
+
+func main() {
+    myInstance := MyType{Value: 10}
+
+    myInstance.SetValue(20)
+    fmt.Println("Value after using Value Receiver:", myInstance.Value) // 출력: 10 (원본 구조체 변경되지 않음)
+
+    myInstance.SetValueWithPointer(30)
+    fmt.Println("Value after using Pointer Receiver:", myInstance.Value) // 출력: 30 (원본 구조체 변경됨)
+}
